9d50072400fed052560dc6eb641379be6f182fe5
beambeambeam <supawitmarayat@gmail.com>
2025-12-04 19:13:06 +0700

    chore: first check and cleanup

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -4,20 +4,21 @@ import "./index.css";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
-	const router = createTanStackRouter({
-		routeTree,
-		scrollRestoration: true,
-		defaultPreloadStaleTime: 0,
-		context: {},
-		defaultPendingComponent: () => <Loader />,
-		defaultNotFoundComponent: () => <div>Not Found</div>,
-		Wrap: ({ children }) => <>{children}</>,
-	});
-	return router;
+  const router = createTanStackRouter({
+    routeTree,
+    scrollRestoration: true,
+    defaultPreloadStaleTime: 0,
+    context: {},
+    defaultPendingComponent: () => <Loader />,
+    defaultNotFoundComponent: () => <div>Not Found</div>,
+    Wrap: ({ children }) => <>{children}</>,
+  });
+  return router;
 };
 
 declare module "@tanstack/react-router" {
-	interface Register {
-		router: ReturnType<typeof getRouter>;
-	}
+  // biome-ignore lint/style/useConsistentTypeDefinitions: create by tanstack start cli.
+  interface Register {
+    router: ReturnType<typeof getRouter>;
+  }
 }
